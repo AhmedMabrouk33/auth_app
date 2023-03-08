6c21b61ac5ec3bf40a6092b7234039aabb3056ab
AhmedMabrouk <ahmedmabrouk3394@gmail.com>
2023-03-08 06:36:19 +0200

    Untrack backend

--- backend/auth-backend/src/api/clientlocation/controllers/clientlocation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * clientlocation controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::clientlocation.clientlocation');
--- backend/auth-backend/src/api/clientlocation/routes/clientlocation.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * clientlocation router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::clientlocation.clientlocation');
