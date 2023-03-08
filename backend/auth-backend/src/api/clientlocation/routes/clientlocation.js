1ec0996a0267f3284ec616f73617c9a900c97893
AhmedMabrouk <ahmedmabrouk3394@gmail.com>
2023-03-08 02:51:35 +0200

    create api end point with relations

---
+++ backend/auth-backend/src/api/clientlocation/routes/clientlocation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * clientlocation router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::clientlocation.clientlocation');
