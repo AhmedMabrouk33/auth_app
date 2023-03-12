9e774a89ee08458e4adca21497b9155b7aaba47f
AhmedMabrouk <ahmedmabrouk3394@gmail.com>
2023-03-12 04:04:10 +0200

    Create Auth models

---
+++ mobile-app/auth_app/lib/data/models/clientmodel.dart
@@ -0,0 +1,235 @@
+import 'imagemodel.dart';
+
+///
+/// Client Model.
+/// Note: This call will use in local service and web services.
+///
+class ClientModel {
+  final int clientId;
+  String phoneNo;
+  String dateOfBirth;
+  UserModel user;
+  List<ClientLocationModel> clientLocation;
+  ImageModel? clientImage;
+
+  ///////////////////////////////////////////////////////////////
+
+  // ************** * Constructor. ************** /
+
+  ClientModel({
+    required this.clientId,
+    required this.phoneNo,
+    required this.dateOfBirth,
+    required this.user,
+    required this.clientLocation,
+    this.clientImage,
+  });
+
+  ///////////////////////////////////////////////////////////////
+
+  // ************** * From web service. ************** /
+  factory ClientModel.fromWebServices(Map<String, dynamic> json,
+      {required String password}) {
+    List<ClientLocationModel> tempClientLocation = [];
+
+    if (json['attributes']['clientlocation']['data'].isNotEmpty) {
+      final int lengthOfJson =
+          json['attributes']['clientlocation']['data'].length;
+
+      for (int index = 0; index < lengthOfJson; index++) {
+        tempClientLocation.add(
+          ClientLocationModel.fromWebService(
+            json['attributes']['clientlocation']['data'],
+          ),
+        );
+      }
+    }
+    return ClientModel(
+      clientId: json['id'],
+      phoneNo: json['attributes']['phoneno'],
+      dateOfBirth: json['attributes']['dateofbirth'],
+      user: UserModel.fromWebService(
+        json['attributes']['user']['data'],
+        isAttributeJson: true,
+        password: password,
+      ),
+      clientImage: json['attributes']['image']['data'] != null
+          ? ImageModel.fromWebService(
+              json['attributes']['image']['data'],
+              isAttributeJson: true,
+            )
+          : null,
+      clientLocation: tempClientLocation,
+    );
+  }
+
+  ///////////////////////////////////////////////////////////////
+
+  // ************** * To web service. ************** /
+  Map<String, dynamic> toWebService() {
+    return {
+      'phoneno': phoneNo,
+      'dateofbirth': dateOfBirth,
+      'image': clientImage != null ? clientImage!.imageId : null,
+      'user': user.userId,
+    };
+  }
+}
+
+///////////////////////////////////////////////////////////////
+
+///
+/// User Model.
+/// Note: This call will use in local service and web services.
+///
+class UserModel {
+  final int userId;
+  String userName;
+  String email;
+  String password;
+  bool isAutoLogin;
+
+  ///////////////////////////////////////////////////////////////
+
+  // ************** * Constructor. ************** /
+  UserModel({
+    required this.userId,
+    required this.userName,
+    required this.email,
+    required this.password,
+    required this.isAutoLogin,
+  });
+
+  ///////////////////////////////////////////////////////////////
+
+  // ************** * From web services. ************** /
+
+  factory UserModel.fromWebService(Map<String, dynamic> json,
+      {required bool isAttributeJson, required String password}) {
+    return UserModel(
+      userId: json['id'] ?? 0,
+      userName: isAttributeJson
+          ? json['attributes']['username'] ?? ''
+          : json['username'] ?? '',
+      email: isAttributeJson
+          ? json['attributes']['email'] ?? ''
+          : json['email'] ?? '',
+      password: password,
+      isAutoLogin: isAttributeJson ? true : false,
+    );
+  }
+
+  ///////////////////////////////////////////////////////////////
+
+  // ************** * To web services. ************** /
+
+  Map<String, dynamic> toWebService({required bool isLogin}) {
+    return isLogin
+        ? {
+            'identifier': email,
+            'password': password,
+          }
+        : {
+            'username': userName,
+            'email': email,
+            'password': password,
+          };
+  }
+
+  ///////////////////////////////////////////////////////////////
+
+  // ************** * From Local service. ************** /
+  factory UserModel.fromLocalServices(Map<String, dynamic> json) {
+    return UserModel(
+      userId: 0,
+      userName: '',
+      email: json['email'],
+      password: json['password'],
+      isAutoLogin: json['flag'],
+    );
+  }
+
+  ///////////////////////////////////////////////////////////////
+
+  // ************** * To Local service. ************** /
+  Map<String, dynamic> toLocalService() {
+    return {
+      'email': email,
+      'password': password,
+      'flag': isAutoLogin,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'UserModel(userId: $userId, userName: $userName, email: $email, password: $password, isAutoLogin: $isAutoLogin)';
+  }
+}
+
+///////////////////////////////////////////////////////////////
+
+///
+/// Client Location Model.
+/// Note: This call will use in local service and web services.
+///
+class ClientLocationModel {
+  final int clientLocationId;
+  String locationName;
+  int buildingNo;
+  int floorNo;
+  String address;
+  String area;
+  String city;
+
+  ///////////////////////////////////////////////////////////////
+
+  // *********** * Constructor. ********** /
+  ClientLocationModel({
+    required this.clientLocationId,
+    required this.locationName,
+    required this.buildingNo,
+    required this.floorNo,
+    required this.address,
+    required this.area,
+    required this.city,
+  });
+
+  ///////////////////////////////////////////////////////////////
+
+  // *********** * From web service. ********** /
+  factory ClientLocationModel.fromWebService(Map<String, dynamic> json) {
+    return ClientLocationModel(
+      clientLocationId: json['id'] ?? 0,
+      locationName: json['attributes']['name'] ?? '',
+      buildingNo: json['attributes']['buildingno'] ?? 0,
+      floorNo: json['attributes']['floorno'] ?? 0,
+      address: json['attributes']['address'] ?? '',
+      area: json['attributes']['area'] ?? '',
+      city: json['attributes']['city'] ?? '',
+    );
+  }
+
+  ///////////////////////////////////////////////////////////////
+
+  // *********** * To web service. ********** /
+
+  ///
+  /// This method will send client location and send relation with client.
+  /// Note: Don't update send location to client api.
+  ///
+  Map<String, dynamic> toWebService({required int clientId}) {
+    return {
+      'name': locationName,
+      'buildingno': buildingNo,
+      'floorno': floorNo,
+      'address': address,
+      'area': area,
+      'city': city,
+      'client': clientId,
+    };
+  }
+
+  ///////////////////////////////////////////////////////////////
+}
+
+///////////////////////////////////////////////////////////////
---
+++ mobile-app/auth_app/lib/data/models/imagemodel.dart
@@ -0,0 +1,40 @@
+import '../../utils/web/constant/network_constants.dart' show BASE_URL;
+
+///
+/// Image Model.
+/// Note: this will call in web service.
+///
+class ImageModel {
+  final int imageId;
+  final String imageUrl;
+
+  ///////////////////////////////////////////////////////////////
+
+  // *********** * Constructor. ********** /
+  ImageModel({
+    required this.imageId,
+    required this.imageUrl,
+  });
+
+  ///////////////////////////////////////////////////////////////
+  
+  // *********** * From web service. ********** /
+  factory ImageModel.fromWebService(Map<String, dynamic> json,
+      {required bool isAttributeJson}) {
+    try {
+      return ImageModel(
+        imageId: json['id'],
+        imageUrl: BASE_URL +
+            (isAttributeJson ? json['attributes']['url'] : json['url']),
+      );
+    } catch (error) {
+      // FIXME: remove this print line.
+      print('Error message from image model is \n$error');
+      return ImageModel(imageId: 0, imageUrl: '');
+    }
+  }
+
+  ///////////////////////////////////////////////////////////////
+}
+
+///////////////////////////////////////////////////////////////
